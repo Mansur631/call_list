38549b27e7c36fb3a7f559228bd2c33dc28c4042
mansu <stgyyavsesmogyy@gmail.com>
2024-04-08 16:58:17 +0300

    Переименовал функцию для адрессов

--- contacts.py
+++ contacts.py
@@ -1,14 +1,14 @@
 import random
 from datetime import datetime, timedelta
 
-# список имен
+"""список имен"""
 def get_random_names():
     names = ['Alice', 'Bob', 'Charlie', 'David', 'Emma', 'Frank', 'Grace', 'Henry', 'Isabel', 'Jack']
     random_names = random.choice(names)
     return random_names
 
 
-# список фамилий
+"""список фамилий"""
 def get_random_surname():
     surnames = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor']
     random_surname = random.choice(surnames)
@@ -16,23 +16,24 @@ def get_random_surname():
 
 
 def get_random_birtday():
-    # список для хранения дат
+    """список для хранения дат"""
     dates = []
 
-    # генерация 10 случайных дат
+    """генерация 10 случайных дат"""
     for _ in range(1):
-        # генерация случайной даты в диапазоне от 2000 до 2002 года
+        """генерация случайной даты в диапазоне от 2000 до 2002 года"""
         random_date = datetime(random.randint(2000, 2002), random.randint(1, 12), random.randint(1, 28))
-        # форматирование даты в нужный формат
+        """форматирование даты в нужный формат"""
         formatted_date = random_date.strftime("%B %d, %Y")
-        # добавление отформатированной даты в список
+        """добавление отформатированной даты в список"""
         dates.append(formatted_date)
 
-    # вывод списка
+    """вывод списка"""
     for date in dates:
         return date
 
 
+"""Список адрессов"""
 def get_random_address():
     address = ['259 Forsyth Rd', '480 Olympic Blvd', '88 Juniper St', '403 Main Rd', '647 Fifth Ln', '989 Peachtree St', '325 First Ave', '996 Forsyth Pkwy', '781 Tenth Ln', '409 Fourth Pkwy' ]
     random_address = random.choice(address)
