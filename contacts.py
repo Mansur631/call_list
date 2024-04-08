e042bc2f3d3368aaac0366a1442f2dc48d9b2b38
mansu <stgyyavsesmogyy@gmail.com>
2024-04-08 16:54:51 +0300

    Добавлен рандомный список адрессов

--- contacts.py
+++ contacts.py
@@ -15,7 +15,6 @@ def get_random_surname():
     return random_surname
 
 
-# список день рождений
 def get_random_birtday():
     # список для хранения дат
     dates = []
@@ -25,7 +24,7 @@ def get_random_birtday():
         # генерация случайной даты в диапазоне от 2000 до 2002 года
         random_date = datetime(random.randint(2000, 2002), random.randint(1, 12), random.randint(1, 28))
         # форматирование даты в нужный формат
-        formatted_date = random_date.strftime("%b %d, %y")
+        formatted_date = random_date.strftime("%B %d, %Y")
         # добавление отформатированной даты в список
         dates.append(formatted_date)
 
@@ -34,4 +33,10 @@ def get_random_birtday():
         return date
 
 
-# print(get_random_names(), get_random_surname(), get_random_birtday())
+def get_random_address():
+    address = ['259 Forsyth Rd', '480 Olympic Blvd', '88 Juniper St', '403 Main Rd', '647 Fifth Ln', '989 Peachtree St', '325 First Ave', '996 Forsyth Pkwy', '781 Tenth Ln', '409 Fourth Pkwy' ]
+    random_address = random.choice(address)
+    return random_address
+
+
+# print(get_random_names(), get_random_surname(), get_random_birtday(), get_random_address())
