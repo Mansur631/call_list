bf0ed2d2461a3752e90e25ae0cf3dab47bcdda97
mansu <stgyyavsesmogyy@gmail.com>
2024-04-08 14:24:59 +0300

    Удалние отчистки кеша

--- find_elements.py
+++ find_elements.py
@@ -12,36 +12,36 @@ class PageElements:
 
     def by_id(self, element_id):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.ID, element_id)))
         return element
 
     def by_class_name(self, class_name):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
         return element
 
     def by_xpath(self, xpath):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
         return element
 
     def by_name(self, name):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, name)))
         return element
 
     def by_text(self, text):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.LINK_TEXT, text)))
         return element
 
     def by_selector(self, selector):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.refresh()
+        # self.driver.refresh()
         element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
         return element
