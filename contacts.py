dbed6ccda822e658d4d8fc837d0159db88a4bbd9
mansu <stgyyavsesmogyy@gmail.com>
2024-04-08 14:23:39 +0300

    Список рандомных имен, фамилий, ДР контактов!

--- contacts.py
+++ contacts.py
@@ -1,35 +1,35 @@
 import random
 from datetime import datetime, timedelta
 
-# Список имен
+# список имен
 def get_random_names():
-    names = ['Alice', 'Bob', 'Charlie', 'David', 'Emma', 'Frank', 'Grace', 'Henry', 'Isabel', 'Jack']
+    names = ['alice', 'bob', 'charlie', 'david', 'emma', 'frank', 'grace', 'henry', 'isabel', 'jack']
     random_names = random.choice(names)
     return random_names
 
 
-# Список фамилий
+# список фамилий
 def get_random_surname():
-    surnames = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor']
+    surnames = ['smith', 'johnson', 'williams', 'jones', 'brown', 'davis', 'miller', 'wilson', 'moore', 'taylor']
     random_surname = random.choice(surnames)
     return random_surname
 
 
-# Список день рождений
+# список день рождений
 def get_random_birtday():
-    # Список для хранения дат
+    # список для хранения дат
     dates = []
 
-    # Генерация 10 случайных дат
+    # генерация 10 случайных дат
     for _ in range(1):
-        # Генерация случайной даты в диапазоне от 2000 до 2002 года
+        # генерация случайной даты в диапазоне от 2000 до 2002 года
         random_date = datetime(random.randint(2000, 2002), random.randint(1, 12), random.randint(1, 28))
-        # Форматирование даты в нужный формат
-        formatted_date = random_date.strftime("%B %d, %Y")
-        # Добавление отформатированной даты в список
+        # форматирование даты в нужный формат
+        formatted_date = random_date.strftime("%b %d, %y")
+        # добавление отформатированной даты в список
         dates.append(formatted_date)
 
-    # Вывод списка
+    # вывод списка
     for date in dates:
         return date
 
